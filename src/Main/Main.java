276397b4c9124f86baceb56deba93063fcffc078
gabriel_hijo <gabriel_hijo@gabriel_hijo-PC.home>
2020-01-09 16:01:32 +0100

    I create an example into the Main class.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,20 @@
 package Main;
 
+import Model.Histogram;
+import Model.Mail;
+import View.HistogramDisplay;
+import View.MailHistogramBuilder;
+import View.MailListReader;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        String fileName = new String("C:\\Users\\gabriel_hijo\\Documents\\NetBeansProjects\\kata4_2020\\src\\email.txt");
+        List<Mail> mailList = MailListReader.read(fileName);
+        Histogram histogram = MailHistogramBuilder.build(mailList);
+        HistogramDisplay histogramDisplay = new HistogramDisplay(histogram);
+        histogramDisplay.execute();
     }
     
 }
